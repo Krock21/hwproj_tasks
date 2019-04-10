434dce8b822456399d2f8a5e416054ec8226f4c8
krock21rus <krock21rus@gmail.com>
2019-04-10 17:05:56 +0300

    basic implements

--- task1/src/main/java/me/hwproj/LazyFactory.java
+++ task1/src/main/java/me/hwproj/LazyFactory.java
@@ -1,4 +1,64 @@
 package me.hwproj;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Supplier;
+
 public class LazyFactory {
+    public static <T> Lazy<T> createSimpleSingleThreadLazy(@NotNull Supplier<T> supplier) {
+        return new Lazy<T>() {
+            T value;
+            boolean isSetted;
+
+            @Override
+            public T get() {
+                if (!isSetted) {
+                    value = supplier.get();
+                    isSetted = true;
+                }
+                return value;
+            }
+        };
+    }
+
+
+    public static <T> Lazy<T> createSimpleMultiThreadLazy(@NotNull Supplier<T> supplier) {
+        return new Lazy<T>() {
+            T value;
+            boolean isSetted;
+
+            @Override
+            public T get() {
+                T answer = null;
+                synchronized (value) {
+                    if (!isSetted) {
+                        value = supplier.get();
+                        isSetted = true;
+                    }
+                    answer = value;
+                }
+                return answer;
+            }
+        };
+    }
+
+    public static <T> Lazy<T> createComplexMultiThreadLazy(@NotNull Supplier<T> supplier) {
+        return new Lazy<T>() {
+            T value;
+            boolean isSetted;
+
+            @Override
+            public T get() {
+                T answer = null;
+                synchronized (value) {
+                    if (!isSetted) {
+                        value = supplier.get();
+                        isSetted = true;
+                    }
+                    answer = value;
+                }
+                return answer;
+            }
+        };
+    }
 }
