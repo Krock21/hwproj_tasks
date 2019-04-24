733cd0d7b60917321bb7690ae15f58ebb0c3cb2e
Maria Tigina <tigmah@mail.ru>
2019-04-24 17:57:12 +0300

    added tests

--- task3/src/test/java/me/hwproj/BoardTest.java
+++ task3/src/test/java/me/hwproj/BoardTest.java
@@ -1,16 +1,87 @@
 package me.hwproj;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
 
+    private Board game;
+
+    @BeforeEach
+    private void gameInit() {
+        game = new Board();
+    }
+
+
     @Test
     void getStatus() {
     }
 
     @Test
-    void makeMove() {
+    void xWinDiagonalGameTest() {
+        game.makeMove(0,0);
+        game.makeMove(0,1);
+        game.makeMove(1,1);
+        game.makeMove(0,2);
+        game.makeMove(2,2);
+        assertEquals(BoardStatus.X_WIN, game.getStatus());
+    }
+
+
+    @Test
+    void oWinDiagonalGameTest() {
+        game.makeMove(2,1);
+        game.makeMove(0,2);
+        game.makeMove(0,1);
+        game.makeMove(1,1);
+        game.makeMove(0,0);
+        game.makeMove(2,0);
+        assertEquals(BoardStatus.O_WIN, game.getStatus());
+    }
+
+
+    @Test
+    void oWinRowGameTest() {
+        game.makeMove(2,2);
+        game.makeMove(0,0);
+        game.makeMove(1,1);
+        game.makeMove(0,1);
+        game.makeMove(2,1);
+        game.makeMove(0,2);
+        assertEquals(BoardStatus.O_WIN, game.getStatus());
+    }
+
+    @Test
+    void xWinColGameTest() {
+        game.makeMove(0,2);
+        game.makeMove(2,1);
+        game.makeMove(0,1);
+        game.makeMove(1,1);
+        game.makeMove(0,0);
+        assertEquals(BoardStatus.X_WIN, game.getStatus());
+    }
+
+    @Test
+    void drawGameTest() {
+        game.makeMove(0,0);
+        game.makeMove(1,0);
+        game.makeMove(0,1);
+        game.makeMove(1,1);
+        game.makeMove(0,2);
+        game.makeMove(1,2);
+        assertEquals(BoardStatus.DRAW, game.getStatus());
+    }
+
+    @Test
+    void doubleClickGameTest() {
+        game.makeMove(0,0);
+        game.makeMove(0,0);
+        game.makeMove(1,0);
+        game.makeMove(0,1);
+        game.makeMove(1,1);
+        game.makeMove(0,2);
+        game.makeMove(1,2);
+        assertEquals(BoardStatus.DRAW, game.getStatus());
     }
 }
