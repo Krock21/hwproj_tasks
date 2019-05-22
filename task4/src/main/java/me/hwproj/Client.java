f759f985ffc009483dcec210375b8e41e0fcec75
krock21rus <krock21rus@gmail.com>
2019-05-22 15:45:40 +0300

    task4 init classes

---
+++ task4/src/main/java/me/hwproj/Client.java
@@ -0,0 +1,24 @@
+package me.hwproj;
+
+public class Client {
+    
+    Client() {
+
+    }
+
+    public void connect() {
+
+    }
+
+    public void disconnect() {
+
+    }
+
+    public void executeList() {
+
+    }
+
+    public void executeGet() {
+
+    }
+}
---
+++ task4/src/main/java/me/hwproj/Server.java
@@ -0,0 +1,16 @@
+package me.hwproj;
+
+public class Server {
+
+    Server() {
+
+    }
+
+    public void start() {
+
+    }
+
+    public void stop() {
+
+    }
+}
