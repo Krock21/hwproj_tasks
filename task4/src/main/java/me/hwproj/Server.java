27169f0e95762576bab4a03dd56c532a660577a8
123a6bcw <ga.karandash@yandex.ru>
2019-05-22 16:32:57 +0300

    finished get and list

--- task4/src/main/java/me/hwproj/Server.java
+++ task4/src/main/java/me/hwproj/Server.java
@@ -1,23 +1,46 @@
 package me.hwproj;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Server {
 
     public static void main(String[] argc) {
+        if (argc.length == 0 || argc.length > 2) {
+            throw new IllegalArgumentException("Daun vvedi argumenti normalno");
+        }
 
+        String pathToDir = argc[0];
+        Server server = new Server(pathToDir);
+        server.start();
+
+        var result = server.list("papka");
+        if (result == null) {
+            System.out.println("null");
+        } else {
+            for (var guy : result) {
+                System.out.println(guy.fileName + " " + guy.isDirectory);
+            }
+        }
+
+        var fileResult = server.get("papka/ralsei.png");
+        if (fileResult == null) {
+            System.out.println("null");
+        } else {
+            System.out.println(fileResult.size);
+        }
     }
 
     private String pathToDir;
 
     public Server(String pathToDir) {
-        this.pathToDir = pathToDir;
+        this.pathToDir = Paths.get(pathToDir).toAbsolutePath().toString();
     }
 
     public void start() {
@@ -26,6 +49,41 @@ public class Server {
 
     public void stop() {
 
+    }
+
+
+    private List<StringAndBoolean> list(String pathName) {
+        var fileList = new ArrayList<StringAndBoolean>();
+
+        Path path = Paths.get(pathToDir, pathName);
+        File file = path.toFile();
+
+        if (!file.exists() || !file.isDirectory()) {
+            return null;
+        }
+
+        try {
+            for (var subFile : Files.walk(path).map(Path::toFile).collect(Collectors.toList())) {
+                boolean isDirectory = subFile.isDirectory();
+                String pathToSubFile = subFile.getAbsolutePath().substring(pathToDir.length());
+
+                fileList.add(new StringAndBoolean(pathToSubFile, isDirectory));
+            }
+
+            return fileList;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    private static class StringAndBoolean {
+        private String fileName;
+        private boolean isDirectory;
+
+        private StringAndBoolean(String fileName, boolean isDirectory) {
+            this.fileName = fileName;
+            this.isDirectory = isDirectory;
+        }
     }
 
     private SizeAndContent get(String pathName) {
