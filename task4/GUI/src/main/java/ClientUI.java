acd097f4429f2e2a28ddd3dd40180a8303b00e2c
123a6bcw <ga.karandash@yandex.ru>
2019-06-02 05:01:49 +0300

    Added initial design stle

--- task4/GUI/src/main/java/ClientUI.java
+++ task4/GUI/src/main/java/ClientUI.java
@@ -1,64 +1,151 @@
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+
+import java.util.Arrays;
 
 /**
  * TODO
  */
 public class ClientUI extends Application {
-        /**
-         * Main stage of the window.
-         */
-        private Stage primaryStage;
+    /**
+     * TODO
+     */
+    private double screenWidth;
 
-        @Override
-        public void start(Stage primaryStage) throws Exception {
-            this.primaryStage = primaryStage;
 
-            primaryStage.setTitle("My FTP client");
+    /**
+     * TODO
+     */
+    private double screenHeight;
 
-            Group root = new Group();
-            Scene scene = new Scene(root);
-            //primaryStage.setFullScreen(true);
-            //primaryStage.setResizable(false);
+    /**
+     * TODO
+     */
+    private static final int FILES_PER_SCREEN = 20;
 
-            primaryStage.setMinWidth(300);
-            primaryStage.setMinHeight(300);
+    /**
+     * TODO
+     */
+    private double fileHeight;
 
-            primaryStage.widthProperty().addListener((observableValue, oldValue, newValue) -> setCurrentWidthToStage(newValue));
-            primaryStage.heightProperty().addListener((observableValue, oldValue, newValue) -> setCurrentHeightToStage(newValue));
+    /**
+     * TODO
+     */
+    private int currentFilesOnScreen;
 
-        /*Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
-        primaryStage.setX(primaryScreenBounds.getMinX());
+    /**
+     * Main stage of the window.
+     */
+    private Stage primaryStage;
+
+    /**
+     * TODO
+     */
+    private VBox content;
+
+    /**
+     * TODO
+     */
+    private double currentWidth;
+    private double currentHeight;
+
+    /**
+     * TODO
+     */
+    private static final int INITIAL_WIDTH = 300;
+    private static final int INITIAL_FILE_ON_SCREEN = 5;
+
+    /**
+     * TODO
+     */
+    private Label[] labels = new Label[FILES_PER_SCREEN];
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+
+        primaryStage.setTitle("My FTP client");
+
+        content = new VBox();
+        Scene scene = new Scene(content);
+        //primaryStage.setFullScreen(true);
+        //primaryStage.setResizable(false);
+
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+        screenWidth = primaryScreenBounds.getWidth();
+        screenHeight = primaryScreenBounds.getHeight();
+        fileHeight = screenHeight / FILES_PER_SCREEN;
+
+        for (int i = 0; i < FILES_PER_SCREEN; i++) {
+            labels[i] = new Label();
+            labels[i].setText("Хуесосина");
+            labels[i].setMinHeight(fileHeight);
+            labels[i].setFont(Font.font(fileHeight));
+        }
+
+        //TODO
+        primaryStage.setMinWidth(INITIAL_WIDTH);
+        primaryStage.setMinHeight(INITIAL_FILE_ON_SCREEN * fileHeight);
+
+        currentWidth = primaryStage.getWidth();
+        currentHeight = primaryStage.getHeight();
+
+        resize();
+
+        content.widthProperty().addListener((observableValue, oldValue, newValue) -> setCurrentWidthToStage(newValue));
+        content.heightProperty().addListener((observableValue, oldValue, newValue) -> setCurrentHeightToStage(newValue));
+
+        /*primaryStage.setX(primaryScreenBounds.getMinX());
         primaryStage.setY(primaryScreenBounds.getMinY());
         primaryStage.setWidth(primaryScreenBounds.getWidth() - 50); //So it would be easy to drag window...
         primaryStage.setHeight(primaryScreenBounds.getHeight() - 50);
          */
 
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        }
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
-        /**
-         * TODO
-         */
-        private void setCurrentWidthToStage(Number newWidth) {
-            primaryStage.setWidth((double) newWidth);
-        }
+    /**
+     * TODO
+     */
+    private void setCurrentWidthToStage(Number newWidth) {
+        //primaryStage.setWidth((double) newWidth);
+        currentWidth = (double) newWidth;
+        resize();
+    }
 
-        /**
-         * TODO
-         */
-        private void setCurrentHeightToStage(Number newHeight) {
-            primaryStage.setHeight((double) newHeight);
-        }
+    /**
+     * TODO
+     */
+    private void setCurrentHeightToStage(Number newHeight) {
+        //primaryStage.setHeight((double) newHeight);
+        currentHeight = (double) newHeight;
+        resize();
+    }
 
-        @Override
-        public void stop() {
-        }
+    /**
+     * TODO
+     */
+    private void resize() {
+        currentFilesOnScreen = (int) ((currentHeight / screenHeight) * FILES_PER_SCREEN);
 
-        public static void main(String[] args) {
-            Application.launch(args);
-        }
+        content.setFillWidth(true);
+        var fileList = content.getChildren();
+        fileList.clear();
+        fileList.addAll(Arrays.asList(labels).subList(0, currentFilesOnScreen));
+    }
+
+    @Override
+    public void stop() {
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
 }
